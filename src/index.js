5ec875341127b0027a9017f0f7bb4715b377266e
Collins <mussocollins@gmail.com>
2022-08-03 06:37:43 +0300

    Implement the Javascript functions to fetch API together with my coding partner Sharon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import './style.css';
-import { fetchList, showList } from './modules/showlist.js';
+import fetchList from './modules/showlist.js';
 
-fetchList();
-const fetchMovies = fetchList();
-showList(fetchMovies);
+// Loading API images in the DOM
+const showSeriesList = async () => {
+  const seriesEntries = await fetchList();
+  const displaySeries = document.querySelector('.watchlist');
+
+  seriesEntries.forEach((entry) => displaySeries.insertAdjacentHTML('beforeend', `
+    <div id='series-list'>
+    <img class="show-image" src="${entry.image.medium}" alt="${entry.name}">
+    <p class="show-name">${entry.name}  <i class="fa fa-heart"></i></P>
+    <button type="submit" class="comment">Comment</button> 
+    </div> 
+  `));
+};
+
+document.addEventListener('DOMContentLoaded', showSeriesList);
