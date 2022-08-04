dff8653cc26dbbc69a70dc7c01ac6bf4df6dd0cc
Collins <mussocollins@gmail.com>
2022-08-04 09:12:09 +0300

    make changes to setup popup window for the comments

--- src/index.js
+++ src/index.js
@@ -17,6 +17,39 @@ const showSeriesList = async () => {
   `;
     displaySeries.append(displayItem);
   }
+  // displaying the popup window
+  const getPopup = async (id) => {
+    const response = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const result = await response.json();
+    const popupList = document.createElement('div');
+    popupList.classList.add('comment-popup');
+    const body = document.querySelector('body');
+    popupList.innerHTML = `
+    <div class='popup-headers'>
+    <img src= ${result.image.medium}>
+    <button id='close-button'>&times;</button>
+    </div>
+    <h4>${result.name}</h4>
+    <p>${result.summary}</p>
+    <div class='series-attributes'>
+    <p>${result.genres}</p>
+    <p>${result.premiered}</p>
+    <p>${result.rating.average}</p>
+    </div>
+    `;
+    body.append(popupList);
+  };
+  const commentBtns = document.querySelectorAll('.comments');
+  commentBtns.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      getPopup(e.target.parentNode.id);
+    });
+  });
+  // close btn
+  const closeBtn = document.getElementById('close-button');
+  closeBtn.addEventListener('click', () => {
+    getPopup.classList.remove('active');
+  });
 };
 document.addEventListener('DOMContentLoaded', showSeriesList);
 // comments
