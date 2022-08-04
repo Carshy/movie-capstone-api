d4aba1fffd50c96a1ca23984fa037b6bd9d39032
Collins <mussocollins@gmail.com>
2022-08-04 06:34:32 +0300

    Add changes to implement comment popup

--- src/index.js
+++ src/index.js
@@ -9,13 +9,14 @@ const showSeriesList = async () => {
   displayItem.classList.add('card-image1');
   for (let entry = 50; entry < 70; entry += 1) {
     displayItem.innerHTML += `
-    <div class="card-image">
+    <div class="card-image" id = ${seriesEntries[entry].id}>
     <img class="show-image" src="${seriesEntries[entry].image.medium}" alt="${seriesEntries[entry].name}">
     <p class="title">${seriesEntries[entry].name}  <i class="fa fa-heart"></i></p>
-    <button type="submit" class="comments">Comments</button> 
+    <button type="submit" class="comments" id='comment-btn'>Comments</button> 
     </div> 
   `;
     displaySeries.append(displayItem);
   }
 };
 document.addEventListener('DOMContentLoaded', showSeriesList);
+// comments
