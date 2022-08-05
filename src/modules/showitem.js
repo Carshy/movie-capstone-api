c404376d77ec115a1805440dcf5f9b428da21deb
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-08-05 12:49:12 +0300

    update modules and footer styling

--- src/modules/showitem.js
+++ src/modules/showitem.js
@@ -32,5 +32,52 @@ const showSeriesList = async () => {
     const likeItem = document.querySelector(`[data-id = '${item.item_id}']`);
     likeItem.innerHTML = `${item.likes}`;
   });
+
+  // displaying the popup window
+  const getPopup = async (id) => {
+    const response = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const result = await response.json();
+    const popupList = document.createElement('div');
+    popupList.classList.add('comment-popup');
+    const body = document.querySelector('body');
+    popupList.innerHTML = `
+    <div class='popup-headers'>
+    <div class='image-header'>
+    <img src= ${result.image.medium}>
+    </div>
+    <span class="close-button close">&times;</span>
+    </div>
+    <div class='series-details'>
+    <h4>${result.name}</h4>
+    <p>${result.summary}</p>
+    </div>
+    <div class='series-attributes'>
+    <p>Genre: (${result.genres})</p>
+    <p>Premiered on: ${result.premiered}</p>
+    <p>Ratings(${result.rating.average})</p>
+    </div>
+    <div class='comment-section'>
+    <input type='text' class='user-name' placeholder='Your name'>
+    <textarea type='text' class='user-comment' placeholder='Your insights'></textarea>
+    <button class='comment-box' type='submit'>Comment</button>
+    </div>
+    `;
+    body.append(popupList);
+    // Close button
+    const close = document.querySelector('.close');
+    close.addEventListener('click', () => {
+    displaySeries.style.filter = 'blur(0)';
+    const body = document.querySelector('body');
+    body.removeChild(body.lastChild);
+    });
+  };
+  // Comment eventlisters
+  const commentBtns = document.querySelectorAll('.comments');
+  commentBtns.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      getPopup(e.target.parentNode.id);
+      displaySeries.style.filter = 'blur(12px)';
+    });
+  });
 };
 export default showSeriesList;
